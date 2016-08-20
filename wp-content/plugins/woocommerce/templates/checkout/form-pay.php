12057d232af461f49e671d554e3fa8575c1cd26c
cucrecords <cucrecords@yahoo.com>
2016-08-20 12:25:11 +0100

    Updated checkout pages
    Changing table header from product to booking

--- wp-content/plugins/woocommerce/templates/checkout/form-pay.php
+++ wp-content/plugins/woocommerce/templates/checkout/form-pay.php
@@ -25,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<table class="shop_table">
 		<thead>
 			<tr>
-				<th class="product-name"><?php _e( 'Product', 'woocommerce' ); ?></th>
+				<th class="product-name"><?php _e( 'Booking', 'woocommerce' ); ?></th>
 				<th class="product-quantity"><?php _e( 'Qty', 'woocommerce' ); ?></th>
 				<th class="product-total"><?php _e( 'Totals', 'woocommerce' ); ?></th>
 			</tr>
